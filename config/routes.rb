4ac0078d7daa1e2c745933e360bf2060f49da34b
ckma23 <curtiskma@gmail.com>
2017-07-24 22:07:33 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup',  to: 'users#new'
+  resources :users
   #get 'static_pages/home'
   #get 'static_pages/help'
   #get 'static_pages/about'
